f81f6c2ca315041437f82dee858ffae18e7d1795
Jose Sotolongo <jose.sotolongo@dexcom.com>
2025-02-27 12:42:19 -0500

    Add project files.

---
+++ CoreRxApi/UsbSerialCommunication.cs
@@ -0,0 +1,21 @@
+﻿using LibUsbDotNet;
+using LibUsbDotNet.LudnMonoLibUsb;
+using System.IO.Ports;
+
+namespace CoreRxApi
+{
+	public class UsbSerialCommunication
+	{
+		public UsbSerialCommunication()
+		{
+
+		}
+
+		public void FindAllPorts()
+		{
+			UsbDevice.ForceLibUsbWinBack = true;
+
+			var list = MonoUsbDevice.AllLibUsbDevices;
+		}
+	}
+}
---
+++ CoreRxApiPoC/Program.cs
@@ -0,0 +1,13 @@
+﻿using CoreRxApi;
+
+namespace CoreRxApiPoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+	        UsbSerialCommunication usbApi = new UsbSerialCommunication();
+            usbApi.FindAllPorts();
+        }
+    }
+}
